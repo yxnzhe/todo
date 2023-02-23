116ee4605551c7ab804ea77e6b59e2100770aa57
yxnzhe <yanzhe2003@gmail.com>
2023-02-23 11:34:33 +0800

    working

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,23 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import './index.css';
+import { useState } from 'react'
+import TodoForm from './components/TodoForm';
+import TaskList from './components/TaskList';
+import EditForm from './components/EditForm';
 
 function App() {
+  const [tasks, setTasks] = useState([]);
+  const [editedTask, setEditedTask] = useState(null);
+  const [isEditing, setIsEditing] = useState(false);
+  const [previousFocus, setPreviousFocus] = useState(null);
+
+  const addTask = (task) => {
+    setTasks(prevState => [...prevState, task]);
+  }
+
+  const deleteTask = (id) => {
+    setTasks(prevState => prevState.filter(task => task.id !== id));
+  }
+
+  const toggleComplete = (id) => {
+    setTasks(prevState => prevState.map(task => (task.id === id ? { ...task, completed: !task.completed } : task)));
+  }
+
+  const updateTask = (task) => {
+    setTasks(prevState => prevState.map(t => (t.id === task.id ? { ...t, name: task.name } : t)))
+    closeEditMode();
+  }
+
+  const closeEditMode = () => {
+    setIsEditing(false);
+    previousFocus.focus();
+  }
+
+  const enterEditMode = (task) => {
+    setEditedTask(task);
+    setIsEditing(true);
+    setPreviousFocus(document.activeElement);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="container">
+      <header>
+        <h1>Plan for Today</h1>
       </header>
+      {
+        isEditing && (
+          <EditForm 
+            editedTask={editedTask}
+            updateTask={updateTask}
+            closeEditMode={closeEditMode}
+          />
+        )
+      }
+      <TodoForm addTask={addTask}/>
+      {tasks && (
+      <TaskList 
+        tasks={tasks}
+        deleteTask={deleteTask}
+        toggleComplete={toggleComplete}
+        enterEditMode={enterEditMode}
+      />)}
     </div>
   );
 }
